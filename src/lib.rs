7847156bc616623d9b6b6693754de4705ac1fb4f
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2024-03-18 11:27:19 -0700

    Optimize imports and re-order impl

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use std::ffi::CStr;
 
 use pam::{pam_hooks, pam_try};
 use pam::constants::{PAM_TEXT_INFO, PamFlag, PamResultCode};
-use pam::constants::PamResultCode::{PAM_AUTH_ERR, PAM_IGNORE, PAM_SUCCESS};
+use pam::constants::PamResultCode::{PAM_AUTH_ERR, PAM_SUCCESS};
 use pam::conv::Conv;
 use pam::module::{PamHandle, PamHooks};
 use rand::random;
@@ -11,6 +11,10 @@ struct PamRandom;
 pam_hooks!(PamRandom);
 
 impl PamHooks for PamRandom {
+    fn acct_mgmt(_pamh: &mut PamHandle, _args: Vec<&CStr>, _flags: PamFlag) -> PamResultCode {
+        PAM_SUCCESS
+    }
+
     fn sm_authenticate(pamh: &mut PamHandle, _args: Vec<&CStr>, _flags: PamFlag) -> PamResultCode {
         let conv = match pamh.get_item::<Conv>() {
             Ok(Some(conv)) => conv,
@@ -27,9 +31,5 @@ impl PamHooks for PamRandom {
             pam_try!(conv.send(PAM_TEXT_INFO, "Randomly failed"));
             PAM_AUTH_ERR
         }
-    }
-
-    fn acct_mgmt(_pamh: &mut PamHandle, _args: Vec<&CStr>, _flags: PamFlag) -> PamResultCode {
-        PAM_SUCCESS
     }
 }
